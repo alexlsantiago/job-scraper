425499a5107a0f630b42a8e4aeaeb3bde7af076e
Alex Santiago <as286@email.sc.edu>
2025-06-19 07:21:25 -0400

    Fix linking to applications

--- scraper.py
+++ scraper.py
@@ -20,15 +20,25 @@ def scrape_jobs(query, location):
         data = res.json()
         raw_jobs = data.get("jobs_results", [])
         
-        # Normalize fields to avoid missing data
         jobs = []
         for job in raw_jobs:
+            # Try to extract a usable job link
+            apply_link = None
+
+            # Option 1: SerpAPI detected extensions
+            if "detected_extensions" in job:
+                apply_link = job["detected_extensions"].get("apply_link")
+
+            # Option 2: Use the link field directly if it exists
+            if not apply_link:
+                apply_link = job.get("link")
+
             jobs.append({
                 "title": job.get("title", "No title"),
                 "company": job.get("company_name", "Not available"),
                 "location": job.get("location", "Not available"),
                 "snippet": job.get("description", "Not available"),
-                "link": job.get("related_links", [{}])[0].get("link", job.get("job_id", "#"))
+                "link": apply_link or "#"
             })
 
         return jobs
